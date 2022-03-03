f122430127a482329249cddbf973d13c9c8d009a
khadim Niass <niassssn@gmail.com>
2022-03-03 23:09:03 +0000

    terminaison des validation cote front

--- public/js/script.validation.js
+++ public/js/script.validation.js
@@ -1,1 +1,56 @@
-const form=document.getElementById('form');
+const form = document.getElementById("form");
+const btnCon = document.getElementById("connexion");
+
+const inputLogin = document.getElementById("login");
+const inputPassword = document.getElementById("password");
+
+const errorLogin = document.getElementById("erorr-login");
+const errorPassword = document.getElementById("error-pwd");
+//réactiver les parametre du form pour envoyer les données...
+
+function showError(input, message) {
+  const formC = input.parentElement;
+  // formC.setAttribute('class','form-control error');
+  formC.className = "form-control error";
+  const small = document.querySelector("small");
+  small.innerText = message;
+}
+function ChampObligatoir(input) {
+  return input.value === "";
+}
+
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(input.value.trim().toLowerCase());
+  //  showError(input,'login doit etre un email');
+}
+
+function checkLength(input, length) {
+  return input.value == length;
+}
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (ChampObligatoir(inputLogin)) {
+    showError(inputLogin, "le login est obligatoire");
+  }
+  if (ChampObligatoir(inputPassword)) {
+    showError(inputPassword, "le mot de pass est requi pour la connxion");
+  }
+  e.preventDefault();
+  if (!checkEmail(inputLogin)) {
+    showError(inputPassword, "le login doit etre un email");
+  }
+  if (!checkLength(inputPassword)) {
+    showError(inputPassword, "le mot de pass doit");
+  }
+
+  if (
+    !ChampObligatoir(inputLogin) &&
+    !ChampObligatoir(inputPassword) &&
+    checkEmail(inputLogin) && //le login c'est un e-mail
+    checkLength(inputPassword) //la longueur est correct
+  ) {
+    e.target.submit();
+  }
+});
--- templates/securite/connexion.html.php
+++ templates/securite/connexion.html.php
@@ -1,41 +1,49 @@
 <?php
-require_once(PATH_VIEWS . "include" . DIRECTORY_SEPARATOR . "header.inc.html.php"); ?>
+require_once(PATH_VIEWS . "include" . DIRECTORY_SEPARATOR . "header.inc.html.php");
+?>
 
 <link rel="stylesheet" href="<?= WEB_ROOT . DIRECTORY_SEPARATOR . "css" . DIRECTORY_SEPARATOR . "connexion.style.css" ?>" media="screen" type="text/css">
 
-    <div class="main">
-        <div class="form-contain">
-            <div class="header-form">
-                <h2>Login form</h2>
-                <span>x</span>
-            </div>
-            <form action="<?= WEB_ROOT?>" id="form" method="POST">
-                <input type="hidden" name="action" value="connexion">
-                <input type="hidden" name="controller" value="securite">
-                <?php if (isset($errors['connexion'])):?>
-                <p style="color:red"><?=$errors['connexion']?></p>
-                <?php endif?>
-                <div class="connect-params">
-                    <div class="login">
+<div class="main">
+    <div class="form-contain">
+        <div class="header-form">
+            <h2>Login form</h2>
+            <span>x</span>
+        </div>
+        <form id="form" action="<?= WEB_ROOT ?>" method="POST">
+            <input type="hidden" name="action" value="connexion">
+            <input type="hidden" name="controller" value="securite">
+
+            <?php if (isset($errors['connexion'])) : ?>
+                <p style="color:red"><?= $errors['connexion'] ?></p>
+            <?php endif ?>
+            <div class="connect-params">
+                <div class="form-control">
+                <div class="login">
                         <input type="text" name="login" placeholder="Login" id="login">
-                        <?php if (isset($errors['login'])):?>
-                          <p style="color:red"><?=$errors['login']?></p>
-                        <?php endif?>
- 
+                        <?php if (isset($errors['login'])) : ?>
+                            <p style="color:red"><?= $errors['login'] ?></p>
+                        <?php endif ?>
                         <img src="<?= WEB_ROOT . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "ic-login.png" ?>" alt="" width="5%" height="1%">
                     </div>
-                    <div class="password">
-                        <input type="password" name="password" placeholder="Password" id="password">
-                        <?php if (isset($errors['password'])):?>
-                          <p style="color:red"><?=$errors['password']?></p>
-                        <?php endif?>
- 
-                        <img src="<?= WEB_ROOT . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "ic-password.png" ?>" alt="" width="5%" height="1%">
-                    </div>
+                    <small></small>
                 </div>
-                <div class="connect_sign-in">
-                    <input type="submit" value="Connexion" id="connexion" name="btnconnexion">
-            </form>
-            <a href="<?=PATH_VIEWS."securite".DIRECTORY_SEPARATOR."inscription.html.php" ?>" id="inscription">S'inscrire pour jouer</a>
+                <div class="form-control">
+                <div class="password">
+                    <input type="password" name="password" placeholder="Password" id="password">
+                    <?php if (isset($errors['password'])) : ?>
+                        <p style="color:red"><?= $errors['password'] ?></p>
+                    <?php endif ?>
 
-
+                    <img src="<?= WEB_ROOT . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "ic-password.png" ?>" alt="" width="5%" height="1%">
+                </div>
+                <small></small>
+                </div>
+            </div>
+            <div class="connect_sign-in">
+                <input type="submit" value="Connexion" id="connexion" name="btnconnexion">
+        </form>
+        <a href="<?= PATH_VIEWS . "securite" . DIRECTORY_SEPARATOR . "inscription.html.php" ?>" id="inscription">S'inscrire pour jouer</a>
+        <?php
+        require_once(PATH_VIEWS . "include" . DIRECTORY_SEPARATOR . "footer.inc.html.php");
+        ?>
